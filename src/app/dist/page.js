42c2cba016a41af973d6698e460acb0d66e91a90
PRAMILA0509 <pramilavijayamani@gmail.com>
2025-08-23 11:44:51 +0530

    feat : setup  authentication

--- src/app/dist/page.js
+++ src/app/dist/page.js
@@ -1,7 +1,14 @@
+"use client";
 "use strict";
 exports.__esModule = true;
+var nextjs_1 = require("@clerk/nextjs");
 var react_1 = require("react");
 var HomePage = function () {
-    return (react_1["default"].createElement("div", null, "HomePage"));
+    return (react_1["default"].createElement("div", null,
+        "HomePage",
+        react_1["default"].createElement(nextjs_1.SignedIn, null,
+            react_1["default"].createElement(nextjs_1.SignOutButton, null)),
+        react_1["default"].createElement(nextjs_1.SignedOut, null,
+            react_1["default"].createElement(nextjs_1.SignInButton, null))));
 };
 exports["default"] = HomePage;
